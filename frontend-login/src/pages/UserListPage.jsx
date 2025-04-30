d7b63b94eb6ecdc3e070f1a86d427ee341251193
Marco Giacomini <giacomini_m.a@hotmail.com>
2025-04-30 20:53:18 -0300

    Rotas privadas, uso do jwt token, exibindo tela de login e lista de usuários

--- frontend-login/src/pages/UserListPage.jsx
+++ frontend-login/src/pages/UserListPage.jsx
@@ -1,30 +1,51 @@
 ﻿import React, { useEffect, useState } from 'react';
-import { fetchUsers } from '../services/api';
+import { fetchUsers, logout } from '../services/api';
 import Pagination from '../components/Pagination';
+import { useNavigate } from 'react-router-dom';
 
 function UserListPage() {
   const [users, setUsers] = useState([]);
   const [total, setTotal] = useState(0);
   const [page, setPage] = useState(1);
   const pageSize = 10;
+  const navigate = useNavigate();
 
   useEffect(() => {
     async function load() {
-      const data = await fetchUsers(page, pageSize);
-      setUsers(data.users);
-      setTotal(data.total);
+      try {
+        const data = await fetchUsers(page, pageSize);
+        setUsers(data.users);
+        setTotal(data.total);
+      } catch {
+        // Se der 401 ou outro erro, force logout
+        await handleLogout();
+      }
     }
     load();
   }, [page]);
 
+  const handleLogout = async () => {
+    await logout();
+    navigate('/login', {
+      replace: true
+    });
+  };
+
   return (
     <div>
-      <h1>Lista de Usuários</h1>
+      <header style={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
+        <h1>Lista de Usuários</h1>
+        <button onClick={handleLogout}>
+          Logout
+        </button>
+      </header>
+
       <ul>
-        {users.map((u) => (
+        {users.map(u => (
           <li key={u.id}>{u.username}</li>
         ))}
       </ul>
+
       <Pagination
         currentPage={page}
         totalCount={total}
