e4d79c3e7d35f889ccc849d6adcaaa1fe8aae602
Marco Giacomini <giacomini_m.a@hotmail.com>
2025-04-29 21:59:51 -0300

    Create project backend

---
+++ ProjetoLogin.API/Data/ApplicationDbContext.cs
@@ -0,0 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using ProjetoLogin.API.Models;
+
+namespace ProjetoLogin.API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
